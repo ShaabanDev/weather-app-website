f8afb4dc7872762381b38a1b9d7d62f729f87fff
Mohamed <qotozer@gmail.com>
2021-11-17 01:25:39 +0200

    editing port number

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const forecast = require("./utils/forecast");
 const app = express();
 
 
-const port = process.env.PORT || 400;
+const port = process.env.PORT || 4000;
 
 // define the public 'folder contains our static pages' path
 const dirPath = path.join(__dirname, "../public");
